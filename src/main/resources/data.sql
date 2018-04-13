4f1800e72598807d4dee195eca23324a24747ae4
Uwe Heber <uwe@heber.it>
2018-04-13 11:15:00 +0200

    adjusted SQL statements to use custom sequence for H2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
-insert into customer values(1, 'Joe', 'Cocker', 'Company');
-insert into customer values(2, 'Elvis', 'Presley', 'Company');
-insert into customer values(3, 'Bob', 'Dylan', 'Company');
-insert into customer values(4, 'Tina', 'turmer', 'Company');
+-- customer data to fill H2 database initially
+insert into customer values(nextval('customer_sequence'), 'Joe', 'Cocker', 'BioLunar');
+insert into customer values(nextval('customer_sequence'), 'Edd', 'Miller', 'MaxCrest');
+insert into customer values(nextval('customer_sequence'), 'Bob', 'Watson', 'FancyMax');
+insert into customer values(nextval('customer_sequence'), 'Ray', 'Turner', 'Bioteric');
+insert into customer values(nextval('customer_sequence'), 'Amy', 'Seagal', 'EcoFaire');
+insert into customer values(nextval('customer_sequence'), 'Jea', 'Snipes', 'Acegrill');
+insert into customer values(nextval('customer_sequence'), 'Sue', 'Bieber', 'Snoogies');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,9 @@
+-- create custom sequence
+create sequence customer_sequence start with 1000;
+
+-- create
 create table customer (
-   id integer not null,
+   id bigint default next value for customer_sequence,
    firstName varchar(100) not null,
    lastName varchar(100) not null,
    company varchar(100) not null,
